fecb4ee1f7e600899e1f81aec46281c37c4e2a22
michael <mike.wang96029@gapp.nthu.edu.tw>
2022-04-23 16:37:55 +0800

    modified setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open('README.txt', 'r') as f:
 
 setup(
     name='torusgrid',
-    version='1.0',
+    version='0.0.1',
     description='periodic fields with definite dimensions equipped with FFT operations',
     author='Michael Wang',
-    author_email='mike.wang96029@gmail.com',
+    author_email='mike.wang96029@gapp.nthu.edu.tw',
     packages=['src'],
     install_requires=['numpy', 'scipy', 'pyfftw', 'matplotlib', 'tqdm'],
     long_description=long_description
